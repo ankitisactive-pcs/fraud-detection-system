ca0c4edbd9de83016b7230958945840d1fe47560
ankitisactive-pcs <ankitisactive@gmail.com>
2026-04-02 21:23:28 +0530

    Added Transaction Entit and Repository Setup

---
+++ src/main/java/com/ankit/fraud_detection/entity/Transaction.java
@@ -0,0 +1,59 @@
+package com.ankit.fraud_detection.entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "transactions")
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String userId;
+
+    private double amount;
+
+    private String location;
+
+    private LocalDateTime timestamp;
+
+    public Transaction() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+}
---
+++ src/main/java/com/ankit/fraud_detection/repository/TransactionRepository.java
@@ -0,0 +1,7 @@
+package com.ankit.fraud_detection.repository;
+
+import com.ankit.fraud_detection.entity.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+}
